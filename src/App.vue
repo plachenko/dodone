feb93824d0b5261277fb17eb04e4da2534297fa4
Denis Perchenko <plachenko@gmail.com>
2020-02-19 22:18:14 -0500

    Adjusting layout implementing new list items

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,63 @@
 <template>
   <div id="app">
     <div id="left">
-      <div class="top">
-        <h1 id="logo">DoDone</h1>
-        <div v-if="projects.length">
-          <div :class="{project: true, current: k == current}" @click="current = k" v-for="(project, k) in projects" :key="k">{{project.title || project.tempTitle}}</div>
+      <div id="left_cont">
+        <div id="left_top">
+          <h1 id="logo">DoDone</h1>
+          <div id="proj_search" v-if="projectsWithTitle">
+            <input type="search" v-model="projSearch" placeholder="Search" />
+          </div>
         </div>
-        <div class="project" @click="addProject()">+ add</div>
+        <div id="project_list">
+          <div
+            @click="current = k" v-for="(project, k) in searchResult"
+            :class="{project: true, current: k == current, temp: !project.title}"
+            :key="k">
+              <span class="proj_title">{{project.title || project.tempTitle}}</span>
+              <span class="item_count" v-if="project.items.length">{{project.items.length}}</span>
+            </div>
+        </div>
+        <div>
+          <div id="addbtn" class="project" @click="addProject()">+ New list</div>
+          <div class="project" @click="save()">save</div>
+        </div>
       </div>
     </div>
+
     <div id="right">
-      <div v-if="projects.length">
+      <div v-if="projects.length" style="flex: 1;">
         <div class="top">
+
           <div id="list_title">
             <h2 v-if="projects[current].title">{{projects[current].title}}</h2>
             <form v-else @submit.prevent="setProjectTitle()">
-              <input id="project_title_inp" autocomplete="off" v-model="titleInp" :placeholder="projects[current].tempTitle" />
+              <input type="text" autocomplete="off" v-model="titleInp" :placeholder="projects[current].tempTitle" />
+              <input type="submit" />
             </form>
           </div>
-          <div id="list_container">
-            <form v-if="projects[current].title" @submit.prevent="addProjectItem()">
-              <input id="item_inp" autocomplete="off" v-model="itemInp" placeholder="add new item" />
-            </form>
+
+        </div>
+        <div id="list_container" style="height: 100%;">
+          <form v-if="projects[current].title" @submit.prevent="addProjectItem()">
+            <input type="text" autocomplete="off" v-model="itemInp" placeholder="add new item" />
+            <input type="submit" />
+          </form>
+          <div v-if="projects[current].items.length">
             <ul>
               <li
                 v-for="(i, k) in projects[current].items"
                 :key="k"
                 :class="{done: i.done}"
-                @click="i.done = !i.done">{{i.txt}} - {{new moment(i.date).format('MM/DD/YYYY')}}</li>
+                @click="i.done = !i.done">{{i.txt}}</li>
             </ul>
           </div>
+          <div v-else style="display: flex; align-items: center; background-color:#0F0; height: 100%; justify-items: center;">
+            <div style="width: 100px; background-color:#F00;">
+              no items.
+            </div>
+          </div>
         </div>
+
       </div>
     </div>
   </div>
@@ -44,6 +71,7 @@ class Project{
   public items = [];
   public id !: symbol;
   public title = "";
+  public proj_search = "";
   public projects = [];
   public tempTitle = "new project"
 
@@ -65,9 +93,9 @@ class Project{
 
   public createItem(text: string, date: object){
     this.items.push({
-      txt: text,
-      done: false,
-      date: date || new Date()
+      txt: text as string,
+      done: false as boolean,
+      date: date || new Date() as object
     })
   }
 }
@@ -78,26 +106,29 @@ export default class App extends Vue {
   private inp = "";
   private current = 0;
   private projects: object[] = [];
+  private projSearch = "";
   private titleInp = "";
   private itemInp = "";
 
-  /*
-  private mounted(){
-    const items = [];
-    let p;
-    for(let i = 0; i < 100; i++){
-      items.push('test');
-      if(i == 99){
-        p = new Project('test', items);
-        this.projects.push(p)
-      }
-    }
+  get searchResult(){
+    return this.projects.filter((item) => {
+      return this.projSearch.toLowerCase().split(' ').every(i => item.title.toLowerCase().includes(i));
+    })
   }
-  */
+
+  get projectsWithTitle(){
+    return this.projects.some(i => i.title.length)
+  }
 
   public addProjectItem(){
-    this.projects[this.current].createItem(this.itemInp);
-    this.itemInp = '';
+    if(this.itemInp){
+      this.projects[this.current].createItem(this.itemInp);
+      this.itemInp = '';
+    }
+  }
+
+  public save(){
+    console.log('saving...');
   }
 
   public setProjectTitle(){
@@ -106,10 +137,11 @@ export default class App extends Vue {
   }
 
   public addProject(){
+    this.projSearch = "";
     const project = new Project();
 
     this.projects.push(project);
-    this.current++;
+    this.current = this.projects.length-1;
   }
 }
 </script>
@@ -118,20 +150,14 @@ export default class App extends Vue {
 *, html{
   margin: 0px;
   padding: 0px;
-}
-html{
-  height: 100%;
-}
-body{
-  height: 100%;
-}
-#logo{
-  text-align: center;
-  border-bottom: 1px solid;
-  width: 100%;
-  padding: 5px 10px;
-  box-sizing: border-box;
-}
+  }
+  html{
+    height: 100%;
+    }
+    body{
+      height: 100%;
+      }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -141,65 +167,133 @@ body{
   width: 100%;
   overflow: hidden;
   }
+
   #left{
-    flex: 1;
     height: 100%;
-    overflow-y: auto;
     flex-basis: 20%;
+    min-width: 170px;
+    border-right: 1px solid;
     }
+    #left_top{
+      width: 100%;
+      }
+      #logo{
+        border-bottom: 1px solid;
+        width: 100%;
+        font-size: 1.4em;
+        padding: 11px;
+        box-sizing: border-box;
+        }
+      #addbtn{
+        text-align: center;
+        border-top: #EEE 2px solid;
+        font-weight: bold;
+        }
+    #proj_search{
+      padding: 10px;
+      border-bottom: 1px solid;
+      }
+      #proj_search input{
+        width: 100%;
+        padding: 10px;
+        box-sizing:border-box;
+        }
+
+    #project_list{
+      flex: 1;
+      width: 100%;
+      overflow-y: auto;
+      }
+      .proj_title{
+        display: block;
+        width: 80%;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        float: left;
+        white-space: nowrap;
+        }
+      .item_count {
+        background-color: #CCC;
+        float: right;
+        display: block;
+        padding: 5px;
+        margin-top: -5px;
+        border-radius: 10px;
+        }
+    #left_cont{
+      display: flex;
+      flex-direction: column;
+      height: 100%;
+      }
+
+      .temp{
+        color:#AAA;
+        }
+
+    .project{
+      border-bottom: #CCC 1px solid;
+      height: 55px;
+      padding: 20px;
+      cursor: pointer;
+      user-select: none;
+      box-sizing: border-box;
+      }
+      .project:hover{
+        background-color:#EEE;
+        }
+      .current{
+        background-color:#EEE;
+        }
+
   #right{
-    background-color:#F0F;
-    overflow-y: auto;
     flex: 1;
     height: 100%;
     flex-basis: 90%;
+    min-width: 350px;
+    overflow: auto;
     }
+    #list_title{
+      border-bottom: #CCC 1px dashed;
+      padding: 10px 10px 10px 10px;
+      }
+      #list_title input[type=text]{
+        padding: 5px 5px 4px 5px;
+        width: 80%;
+        box-sizing: border-box;
+        }
+      #list_title input[type=submit]{
+        width: 19%;
+        margin-left: 1%;
+        padding: 5px 5px 4px 5px;
+        }
+      #list-title h2{
+        display: inline-block;
+        margin-left: 20px;
+        }
+    #list_container{
+      padding: 0px 10px;
+      }
+      #list_container input[type=text]{
+        width: 80%;
+        box-sizing: border-box;
+        padding: 10px;
+        margin: 10px 0px;
+        }
+      #list_container input[type=submit]{
+        width: 19%;
+        margin-left: 1%;
+        padding: 10px;
+        }
 
-li{
-  cursor: pointer;
-  user-select: none;
-  border-bottom: 1px solid;
-  padding: 20px 0px;
-}
-.done{
-  text-decoration: line-through;
-}
-.current{
-  background-color:#CCC !important;
-  }
-.project{
-  /* text-align: center; */
-  border-bottom: #CCC 1px solid;
-  padding: 20px;
-  cursor: pointer;
-  user-select: none;
-  box-sizing: border-box;
-  }
-  .project:hover{
-    background-color:#EEE;
-    }
-  .project > .current{
-    background-color:#CCC;
-    }
-#list_title{
-  border-bottom: 1px dashed;
-  padding: 10px;
-}
-#list_container{
-  padding: 0px 30px;
-}
+    li{
+      cursor: pointer;
+      user-select: none;
+      border-bottom: 1px solid;
+      padding: 20px 0px;
+      }
+      .done{
+        text-decoration: line-through;
+        }
 
-#project_title_inp{
 
-  padding:5px;
-  width: 100%;
-  box-sizing: border-box;
-}
-
-#item_inp{
-  width: 100%;
-  box-sizing: border-box;
-  padding: 10px;
-  margin: 10px 0px;
-}
 </style>
