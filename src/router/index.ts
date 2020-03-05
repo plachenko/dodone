52a7fa6fe76dd059e2500be11d72d82516c20699
Denis Perchenko <plachenko@gmail.com>
2020-03-05 00:44:41 -0500

    oauth updates

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 // import Home from '../views/Home.vue'
+import About from '../views/About.vue'
 import OAuth from '../views/OAuth.vue'
 import VApp from '../views/VApp.vue'
 
@@ -10,22 +11,26 @@ const routes = [
   {
     path: '/',
     name: 'VApp',
-    alias: '/dendo',
-    children:[
-      {
-        path: 'oauth',
-        component: OAuth
-      }
-    ],
-    component: VApp,
+    // alias: '/dendo',
+    component: VApp
+  },
+  {
+    path: '/dendo',
   },
   {
     path: '/about',
     name: 'About',
+    children:[
+      {
+        path: '/oauth',
+        component: OAuth
+      }
+    ],
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: OAuth
   }
 ]
 
